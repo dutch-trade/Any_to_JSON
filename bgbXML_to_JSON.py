ae1b978bca3dc31c87f9d4d67a6b85c5145000ce
Axel Boerrigter <147532556+axel-boerrigter@users.noreply.github.com>
2023-11-16 23:59:45 +0100

    Create bgbXML_to_JSON.py
    This converts all xml files in the bgb folder from dropbox into one large json dictionary called 'bgb_json'. It is one object called "bgb_voyages", for the eventuality that we combine them all into one json, then every set of voyage sources can be an object. within the object bgb_voyages are objects for every voyage, containing key-value pairs for every piece of information. For the cargo, the values are arrays, where the index of every array corresponds to the others, ie: Quantities[1] corresponds to Products[1] and Specification[1] etc.

---
+++ bgbXML_to_JSON.py
@@ -0,0 +1,80 @@
+from bs4 import BeautifulSoup
+import json
+import os
+
+def parse_xml_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    return parse_xml_content(content)
+
+def parse_xml_content(content):
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Extract information from the first table
+    first_table = soup.find('table')
+    first_table_data = {}
+    if first_table:
+        for row in first_table.find_all('tr'):
+            columns = row.find_all(['th', 'td'])
+            if columns:
+                key = columns[0].text.strip()
+                value = columns[1].text.strip()
+                first_table_data[key] = value
+
+    # Extract information from the second table
+    second_table_data = {"Quantities": [], "Products": [], "Specification": [], "Value Dutch guilders": [], "Value Indian guilders": []}
+    second_table = soup.find_all('table')
+    if second_table:
+        second_table = second_table[1]
+        for row in second_table.find_all('tr', class_='odd'):
+            columns = row.find_all('td')
+            second_table_data["Quantities"].append(columns[0].text.strip())
+            second_table_data["Products"].append(columns[1].text.strip())
+            second_table_data["Specification"].append(columns[2].text.strip())
+            second_table_data["Value Dutch guilders"].append(columns[3].text.strip())
+            second_table_data["Value Indian guilders"].append(columns[4].text.strip())
+
+    # Create the final dictionary entry
+    entry = {
+        'Number': first_table_data.get('Number', ''),
+        'Book year': first_table_data.get('Book year', ''),
+        'Source': first_table_data.get('Source', ''),
+        'Folio number': first_table_data.get('Folio number', ''),
+        'Ship name': first_table_data.get('Ship name', ''),
+        'Departure date': first_table_data.get('Departure date', ''),
+        'Departure place and region': first_table_data.get('Departure place and region', ''),
+        'Arrival date': first_table_data.get('Arrival date', ''),
+        'Arrival place and region': first_table_data.get('Arrival place and region', ''),
+        'Total value Dutch guilders': first_table_data.get('Total value Dutch guilders', ''),
+        'Total value Indian guilders': first_table_data.get('Total value Indian guilders', ''),
+        'Remarks Voyage': first_table_data.get('Remarks Voyage', ''),
+        'Voyage in DAS': first_table_data.get('Voyage in DAS', ''),
+        'Quantities': second_table_data["Quantities"],
+        'Products': second_table_data["Products"],
+        'Specification': second_table_data["Specification"],
+        'Value Dutch guilders': second_table_data["Value Dutch guilders"],
+        'Value Indian guilders': second_table_data["Value Indian guilders"]
+    }
+
+    return entry
+
+def process_all_xml_files(directory_path):
+    bgb_voyages = []  # List to store results for each voyage
+
+    # Get a list of all XML files in the directory
+    xml_files = [f for f in os.listdir(directory_path) if f.endswith('.xml')]
+
+    # Process each XML file
+    for xml_file in xml_files:
+        file_path = os.path.join(directory_path, xml_file)
+        parsed_data = parse_xml_file(file_path)
+        bgb_voyages.append(parsed_data)
+
+    # Write the results to a file named "bgb_json.json"
+    with open('bgb_json.json', 'w', encoding='utf-8') as output_file:
+        json.dump({"bgb_voyages": bgb_voyages}, output_file, indent=2)
+
+# Example usage with a directory path
+directory_path = '/Users/axel22/Dropbox/trade_data/bgb'
+process_all_xml_files(directory_path)
