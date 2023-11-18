db6138bec1f9e9db34579a824badb7537164b808
Axel Boerrigter <147532556+axel-boerrigter@users.noreply.github.com>
2023-11-18 13:26:35 +0100

    Update merge_dac.py
    adjusted for when headers have different names in the two files

--- merge_dac.py
+++ merge_dac.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
 # Read the CSV files
-file1 = '/users/axel22/dropbox/trade_data/dac/csv/3316574_DutchAtlantic_accdb/DutchAtlantic.accdb.JLogfinal.csv'
-file2 = '/users/axel22/dropbox/trade_data/dac/csv/3316574_DutchAtlantic_accdb/DutchAtlantic.accdb.migrants.csv'
+file1 = '/users/axel22/documents/trade project/data harmonisation/copies of raw data csv files/JLog+migrants+cargo.csv'
+file2 = '/users/axel22/dropbox/trade_data/dac/csv/3316574_DutchAtlantic_accdb/DutchAtlantic.accdb.destination.csv'
 
 df1 = pd.read_csv(file1)
 df2 = pd.read_csv(file2)
 
+# Specify the columns to merge on
+left_on_column = 'Place'  # Column in the first file
+right_on_column = 'DestinationID'  # Column in the second file
+
 # merge the dataframes based on "JourneylogID"
-result = pd.merge(df1, df2, on='JourneylogID',how='left')
+result = pd.merge(df1, df2, left_on=left_on_column, right_on=right_on_column,how='left')
 
 # Save the result to a new CSV file
-result.to_csv('/users/axel22/documents/trade project/data harmonisation/copies of raw data csv files/jlogfinal+migrants.csv', index=False)
+result.to_csv('/users/axel22/documents/trade project/data harmonisation/copies of raw data csv files/Jlog+migrants+cargo+destinations.csv', index=False)
